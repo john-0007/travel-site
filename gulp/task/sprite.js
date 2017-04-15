0a00d40756ed05cafe0555cacc391cbce21e902d
John <Xohnjoseph@gmail.com>
2017-04-15 15:56:14 +0700

    added browser fallback

--- gulp/task/sprite.js
+++ gulp/task/sprite.js
@@ -5,6 +5,11 @@ var gulp=require("gulp"),
     svg2png=require("gulp-svg2png");
 
 var config={
+    shape:{
+      spacing:{
+          padding:1
+      }  
+    },
     mode:{
         css:{
             variables:{
